128755cc57de00f07785c895bc7e4ad442b6aef5
Nathan Osman <nathan@quickmediasolutions.com>
2024-08-14 23:36:18 -0700

    Fix math issues with pulse effect.

--- leds/effects/pulse.go
+++ leds/effects/pulse.go
@@ -22,18 +22,16 @@ func (p *pulseEffect) Render(
 	elapsed time.Duration,
 	region Region,
 ) (time.Duration, bool) {
-	x := float64(elapsed) / float64(time.Second)
-	x -= float64(p.period) / 2
-	x *= math.Pi
-	x /= float64(p.period)
-	x = math.Sin(x)
-	x /= 2
-	x += 0.5
-	r, g, b, _ := p.color.RGBA()
+	var (
+		period     = float64(p.period) / float64(time.Second)
+		x          = float64(elapsed) / float64(time.Second)
+		f          = math.Sin((2*x-period/2)*math.Pi/period)*.5 + .5
+		r, g, b, _ = p.color.RGBA()
+	)
 	region.SetAllPixels(&color.RGBA{
-		R: uint8(float64(r) * x * 256),
-		G: uint8(float64(g) * x * 256),
-		B: uint8(float64(b) * x * 256),
+		R: uint8(float64(r/256) * f),
+		G: uint8(float64(g/256) * f),
+		B: uint8(float64(b/256) * f),
 	})
 	return 0, true
 }
