a6f482b12d9b54e7cef1e2a371eaa2ebcba22107
Nathan Osman <nathan@quickmediasolutions.com>
2024-08-17 18:55:56 -0700

    Fix transition algorithm to use RGB (instead of HSV).

--- leds/effects/transition.go
+++ leds/effects/transition.go
@@ -2,28 +2,28 @@ package effects
 
 import (
 	"errors"
+	"image/color"
 	"time"
-
-	"github.com/crazy3lf/colorconv"
 )
 
 type TransitionEffect struct {
 	FromColor  Color    `json:"from_color"`
 	ToColor    Color    `json:"to_color"`
 	Duration   Duration `json:"duration"`
-	hS, sS, vS float64
-	hD, sD, vD float64
+	rS, gS, bS uint16
+	rD, gD, bD uint16
 }
 
 func (t *TransitionEffect) Init(region Region) error {
 	if t.Duration == 0 {
 		return errors.New("duration cannot be 0")
 	}
-	t.hS, t.sS, t.vS = colorconv.ColorToHSV(t.FromColor)
-	hE, sE, vE := colorconv.ColorToHSL(t.ToColor)
-	t.hD = hE - t.hS
-	t.sD = sE - t.sS
-	t.vD = vE - t.vS
+	rS, bS, gS, _ := t.FromColor.RGBA()
+	rE, gE, bE, _ := t.ToColor.RGBA()
+	t.rS, t.gS, t.bS = uint16(rS), uint16(gS), uint16(bS)
+	t.rD = uint16(rE) - t.rS
+	t.gD = uint16(gE) - t.gS
+	t.bD = uint16(bE) - t.bS
 	return nil
 }
 
@@ -38,11 +38,10 @@ func (t *TransitionEffect) Render(
 	if !cont {
 		f = 1
 	}
-	c, _ := colorconv.HSVToColor(
-		t.hS+f*t.hD,
-		t.sS+f*t.sD,
-		t.vS+f*t.vD,
-	)
-	region.SetAllPixels(c)
+	region.SetAllPixels(color.RGBA64{
+		R: t.rS + uint16(f*float64(t.rD)),
+		G: t.gS + uint16(f*float64(t.gD)),
+		B: t.bS + uint16(f*float64(t.bD)),
+	})
 	return 0, cont
 }
